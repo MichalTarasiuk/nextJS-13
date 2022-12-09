6dfeb2143612f1110e7ba5cf2784b4ff9abfd2a4
Michał Tarasiuk <69385846+MichalTarasiuk@users.noreply.github.com>
2022-12-09 19:27:36 +0100

    configure tailwind

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+import './globals.css';
+
+export default function RootLayout({children}: {children: React.ReactNode}) {
+  return (
+    <html>
+      <head>
+        <title>Nested layouts by example</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
